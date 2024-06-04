ae015c56fc6afc4ee48eb3ba70a49b062f3cd658
Priyanshi <piyuklsrya26@gmail.com>
2024-06-04 17:21:26 +0530

    Completed herosection

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-scroll';
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <section className='hero'>
+      <img src='/restaurant.jpg' alt='restaurant' />
 
-export default HeroSection
+      <div className='item'>
+        <h3>Dream Maker</h3>
+
+        <div>
+          <h3>Your personal dream maker</h3>
+
+          <p>
+            We believe that it is all about the BIG DREAMS and the samll
+            details!
+          </p>
+
+          <Link to='contact' spy={true} smooth={true} duration={500} className='book-now'>
+            Book Now
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
