dfb917ede692d6f76bd4f8054704eff43f59d8a7
Priyanshi <piyuklsrya26@gmail.com>
2024-06-04 17:08:25 +0530

    Completed navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { Link } from 'react-scroll';
+
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav>
-      Navbar
+      <div className='logo'>EventLy</div>
+
+      <div className={showMenu ? 'navLinks showmenu' : 'navLinks'}>
+        <div className='links'>
+          <Link to='landing' spy={true} smooth={true} duration={500}>
+            Home
+          </Link>
+          <Link to='services' spy={true} smooth={true} duration={500}>
+            Services
+          </Link>
+          <Link to='about' spy={true} smooth={true} duration={500}>
+            About
+          </Link>
+          <Link to='contact' spy={true} smooth={true} duration={500}>
+            Contact
+          </Link>
+        </div>
+      </div>
+
+      <div className='hamburger' onClick={() => setShowMenu(!showMenu)}>
+        <GiHamburgerMenu />
+      </div>
     </nav>
   );
 };
