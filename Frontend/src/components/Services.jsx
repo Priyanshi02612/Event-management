e2df4309d230692b0845d6114b7c7610fa507209
Priyanshi <piyuklsrya26@gmail.com>
2024-06-05 11:47:13 +0530

    Completed Services component

--- Frontend/src/components/Services.jsx
+++ Frontend/src/components/Services.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
 
 const Services = () => {
-  return (
-    <div>Services</div>
-  )
-}
+  const services = [
+    {
+      id: 1,
+      url: '/birthday.jpg',
+      title: 'Birthday Planning',
+    },
+    {
+      id: 2,
+      url: '/anniversary.jpg',
+      title: 'Anniversary Planning',
+    },
+    {
+      id: 3,
+      url: '/camping.jpg',
+      title: 'Camping Trip Planning',
+    },
+    {
+      id: 4,
+      url: '/gamenight.jpg',
+      title: 'Game Night Planning',
+    },
+    {
+      id: 5,
+      url: '/party.jpg',
+      title: 'Party Planning',
+    },
+    {
+      id: 6,
+      url: '/wedding.jpg',
+      title: 'Wedding Planning',
+    },
+  ];
 
-export default Services
+  return (
+    <div className='services container'>
+      <h2>OUR SERVICES</h2>
+
+      <div className='banner'>
+        {services.map((service) => {
+          return (
+            <div key={service.id} className='item'>
+              <h3>{service.title}</h3>
+
+              <img src={service.url} alt={service.title} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
