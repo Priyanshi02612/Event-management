0d05bc47e37482b6446476d1cd2985130b6892d9
Priyanshi <piyuklsrya26@gmail.com>
2024-06-05 11:46:50 +0530

    Completed About component

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className='about container'>
+      <h4>Events</h4>
 
-export default About
+      <h2>ABOUT</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto illo
+        maxime non sed, accusantium numquam porro cum quas eveniet minima
+        mollitia nemo quo molestias rerum deleniti. Ipsum earum eveniet
+        explicabo eos dolores culpa quisquam, quo voluptate natus quos quia
+        beatae provident molestiae aut itaque eius quam nulla! Placeat magni
+        itaque natus nam, dolorum quidem ea veniam iure pariatur suscipit.
+        Maxime, nihil dolor facilis ad ipsam unde optio cupiditate praesentium
+        aspernatur adipisci libero esse, perferendis nam, eos nesciunt et. Aut
+        maiores iusto, libero ad blanditiis ea tempora perferendis id facilis
+        sit, doloribus quasi! Optio aperiam et inventore. Voluptate distinctio
+        architecto est.
+      </p>
+      
+    </section>
+  );
+};
+
+export default About;
